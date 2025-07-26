34e4f00a7dbbed98d9ccaeabcf4142b4f7cf4cf6
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-07-26 00:09:23 -0300

    massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -13,17 +13,33 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Eos V-Neck Hoodie'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        let produto = 'Ariel Roll Sleeve Sweatshirt'
+        produtosPage.visitarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-        
+        let produto = 'Ariel Roll Sleeve Sweatshirt'
+        let qtd = 1
+        produtosPage.visitarProduto(produto)
+        produtosPage.addProdutoCarrinho('M', 'Green', qtd)
+
+        cy.get('.woocommerce-message').should('contain', '“Ariel Roll Sleeve Sweatshirt” foi adicionado no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.visitarProduto(dados[1].nomeProduto)
+        produtosPage.addProdutoCarrinho(dados[1].tamanho, dados[1].cor, dados[1].quantidade)
+
+        cy.get('.woocommerce-message').should('exist')    
+        })
     });
 });
